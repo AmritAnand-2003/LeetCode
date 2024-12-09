02361aaf633c48924ecd78bc9978964f3c53af03
Amrit Anand <78211070+AmritAnand-2003@users.noreply.github.com>
2024-12-09 18:02:21 +0530

    Time: 9 ms (67.58%), Space: 126.5 MB (68.49%) - LeetHub

--- 3427-special-array-ii/3427-special-array-ii.cpp
+++ 3427-special-array-ii/3427-special-array-ii.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
-        set<int> difParity;
-        for(int i = 0;i < nums.size() - 1;i++){
-            if(nums[i] % 2 == nums[i + 1] % 2){
-                difParity.insert(i);
+        // set<int> difParity;
+        vector<int> prefix(nums.size());
+        for(int i = 1;i < nums.size();i++){
+            if(nums[i] % 2 == nums[i - 1] % 2){
+                prefix[i] = 1;
                 // cout<<i<<endl;
             }
+            prefix[i] += prefix[i - 1];
         }
         vector<bool> ans(queries.size(), true);
         for(int i = 0;i < queries.size();i++){
             int s = queries[i][0], d = queries[i][1];
-            auto it = difParity.lower_bound(s);
-            if(it != difParity.end() && *it < d){
+            if(prefix[d] != prefix[s]){
                 ans[i] = false;
             }
         }
