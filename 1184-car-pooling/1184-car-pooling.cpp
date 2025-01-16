a23fdb6416c06fbad473c11901c46e9bf01e38c1
Amrit Anand <78211070+AmritAnand-2003@users.noreply.github.com>
2025-01-16 23:37:03 +0530

    Time: 0 ms (100%), Space: 14.7 MB (26.28%) - LeetHub

--- 1184-car-pooling/1184-car-pooling.cpp
+++ 1184-car-pooling/1184-car-pooling.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        map<int, int> line;
+        vector<int> line(1001);
         for(auto t : trips){
             line[t[1]] += t[0];
             line[t[2]] -= t[0];
         }
         int cur = 0;
-        for(auto l : line){
-            cur += l.second;
+        for(int i = 0;i < line.size();i++){
+            cur += line[i];
             if(cur > capacity){
                 return false;
             }
