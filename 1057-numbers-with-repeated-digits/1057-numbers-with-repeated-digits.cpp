2c1cdaac5e8977fca5cf8e6765cff27260f6e26e
Amrit Anand <78211070+AmritAnand-2003@users.noreply.github.com>
2025-01-17 16:05:29 +0530

    Time: 5 ms (55.86%), Space: 8.2 MB (47.59%) - LeetHub

--- 1057-numbers-with-repeated-digits/1057-numbers-with-repeated-digits.cpp
+++ 1057-numbers-with-repeated-digits/1057-numbers-with-repeated-digits.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
     int dp[12][1025][2];
-    int solve(int ind, int repeated, int taken, bool edge, vector<int>& num){
+    int solve(int ind, int taken, bool edge, vector<int>& num){
         if(ind < 0){
-            return repeated;
+            return taken > 1 ? 1 : 0;
         }
-        if(!edge && dp[ind][taken][repeated] != -1){
-            return dp[ind][taken][repeated];
+        if(dp[ind][taken][edge] != -1){
+            return dp[ind][taken][edge];
         }
         int up_limit = edge ? num[ind] : 9;
         int ans = 0;
         for(int n = 0; n <= up_limit;n++){
-            bool curRepeated = repeated;
-            if(!curRepeated && (taken & (1 << n))){
-                curRepeated = 1;
+            if((taken & (1 << n)) == 0){
+                int nextTaken = (taken != 0 || n != 0) ? (taken | (1 << n)) : 0;
+                ans += solve(ind - 1, nextTaken, (edge ? n == up_limit : false), num);
             }
-            int nextTaken = (taken != 0 || n != 0) ? (taken | (1 << n)) : 0;
-            ans += solve(ind - 1, curRepeated, nextTaken, (edge ? n == up_limit : false), num);
         }
-        if(!edge){
-            dp[ind][taken][repeated] = ans;
-        }
-        return ans;
+        return dp[ind][taken][edge] = ans;
     }
     int numDupDigitsAtMostN(int n) {
+        int temp = n;
         memset(dp, -1, sizeof(dp));
         vector<int> num;
-        while(n){
-            num.push_back(n % 10);
-            n /= 10;
+        while(temp){
+            num.push_back(temp % 10);
+            temp /= 10;
         }
-        return solve(num.size() - 1, 0, 0, true, num);
+        int rep = solve(num.size() - 1, 0, true, num);
+        return n - rep;
     }
 };
