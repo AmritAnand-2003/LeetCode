9d3c1815948a17a9c1696afb6f91f45367567ce0
Amrit Anand <78211070+AmritAnand-2003@users.noreply.github.com>
2025-02-10 14:21:39 +0530

    Time: 115 ms (92.69%), Space: 42.8 MB (79.75%) - LeetHub

--- 2583-divide-nodes-into-the-maximum-number-of-groups/2583-divide-nodes-into-the-maximum-number-of-groups.cpp
+++ 2583-divide-nodes-into-the-maximum-number-of-groups/2583-divide-nodes-into-the-maximum-number-of-groups.cpp
@@ -25,30 +25,38 @@ public:
         return true;
     }
 
-    int magnificentSets(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> adj(n);
-        vector<int> col(n, -1);
-        vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
-        for(auto& e : edges){
-            int s = e[0] - 1, d = e[1] - 1;
-            adj[s].push_back(d);
-            adj[d].push_back(s);
-            dist[s][d] = 1;
-            dist[d][s] = 1;
-        }
-        int ans = 0;        
-        for(int i = 0;i <n;i++){
-            dist[i][i] = 0;
-        }
-        for(int k = 0;k < n;k++){
-            for(int i = 0;i < n;i++){
-                for(int j = 0;j < n;j++){
-                    if(dist[i][k] != INT_MAX && dist[k][j] != INT_MAX && dist[i][j] > dist[i][k] + dist[k][j]){
-                        dist[i][j] = dist[i][k] + dist[k][j];
+    int depthOfComponent(vector<vector<int>>& adj, vector<int>& component){
+        int max_depth = 0;
+        queue<int> q;
+        vector<int> dist(adj.size(), -1);
+        for(auto s : component){
+            dist.assign(adj.size(), -1);
+            q.push(s);
+            dist[s] = 0;
+            while(!q.empty()){
+                int cur = q.front();
+                q.pop();
+                for(auto nbr : adj[cur]){
+                    if(dist[nbr] == -1){
+                        dist[nbr] = dist[cur] + 1;
+                        max_depth = max(max_depth, dist[nbr]);
+                        q.push(nbr);
                     }
                 }
             }
         }
+        return max_depth + 1;
+    }
+
+    int magnificentSets(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> adj(n);
+        vector<int> col(n, -1);
+        for(auto& e : edges){
+            int s = e[0] - 1, d = e[1] - 1;
+            adj[s].push_back(d);
+            adj[d].push_back(s);
+        }
+        int ans = 0;        
         vector<vector<int>> components;
         for(int i = 0;i < n;i++){
             if(col[i] == -1){
@@ -60,16 +68,7 @@ public:
         }
         int group_count = 0;
         for(auto& c : components){
-            cout<<"here";
-            int cur = INT_MIN;
-            for(auto v : c){
-                for(int i = 0;i < n;i++){
-                    if(dist[v][i] != INT_MAX){
-                        cur = max(cur, dist[v][i]);
-                    }
-                }
-            }
-            group_count += cur + 1;
+            group_count += depthOfComponent(adj, c);
         }
         return group_count;
     }
