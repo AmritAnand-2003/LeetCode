238d2f25df8daee3f6d1e5c2e12a0fb29f0f102d
Amrit Anand <78211070+AmritAnand-2003@users.noreply.github.com>
2025-02-23 16:19:31 +0530

    Time: 0 ms (100%), Space: 28.2 MB (76.05%) - LeetHub

--- 0925-construct-binary-tree-from-preorder-and-postorder-traversal/0925-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
+++ 0925-construct-binary-tree-from-preorder-and-postorder-traversal/0925-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
@@ -15,26 +15,18 @@ public:
         if(preEnd < preStart || postEnd < postStart){
             return NULL;
         }
-        int preRoot = preStart;
         int sz = preEnd - preStart;
         TreeNode* cur = new TreeNode(preorder[preStart]);
-        map<int, int> hash;
         for(int ind = 0; ind < sz; ind++){
-            hash[preorder[preStart + ind + 1]]++;
-            hash[postorder[postStart + ind]]--;
-            if(hash.find(postorder[postStart + ind]) != hash.end() && hash[postorder[postStart + ind]] == 0){
-                hash.erase(postorder[postStart + ind]);
-            }
-            if(hash.find(preorder[preStart + ind + 1]) != hash.end() && hash[preorder[preStart + ind + 1]] == 0){
-                hash.erase(preorder[preStart + ind + 1]);
-            }
-            if(hash.size() == 0 && postorder[postStart + ind] == preorder[preStart + 1]){
+            if(postorder[postStart + ind] == preorder[preStart + 1]){
                 cur->left = solve(preorder, preStart + 1, preStart + ind + 1, postorder, postStart, postStart + ind);
                 cur->right = solve(preorder, preStart + ind + 2, preEnd, postorder, postStart + ind + 1, postEnd);
+                break;
             }
         }
         return cur;
     }
+
     TreeNode* constructFromPrePost(vector<int>& preorder, vector<int>& postorder) {
         return solve(preorder, 0, preorder.size() - 1, postorder, 0,  postorder.size() - 1);
     }
