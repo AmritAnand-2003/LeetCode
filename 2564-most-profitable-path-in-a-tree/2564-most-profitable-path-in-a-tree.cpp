cbb1840eed5e62eba1ad435143305387680a3d8b
Amrit Anand <78211070+AmritAnand-2003@users.noreply.github.com>
2025-02-25 19:23:28 +0530

    Time: 95 ms (86.02%), Space: 149.5 MB (80.93%) - LeetHub

--- 2564-most-profitable-path-in-a-tree/2564-most-profitable-path-in-a-tree.cpp
+++ 2564-most-profitable-path-in-a-tree/2564-most-profitable-path-in-a-tree.cpp
@@ -1,69 +1,46 @@
 class Solution {
 public:
-    vector<int> path(int cur_v, int par, vector<vector<int>>& graph, vector<int>& cur_path){
-        if(cur_v == 0){
-            return cur_path;
-        }
-        for(auto ngbr: graph[cur_v]){
-            if(ngbr != par){
-                cur_path.push_back(ngbr);
-                auto p = path(ngbr, cur_v, graph, cur_path);
-                if(!p.empty()){
-                    return p;
-                }
-                cur_path.pop_back();
-            }
-        }
-        return {};
-    }
-    int mostProfitablePath(vector<vector<int>>& edges, int bob, vector<int>& amount) {
-        int n = amount.size();
-        vector<vector<int>> graph(n);
+    int mostProfitablePath(vector<vector<int>>& edges, int bob, vector<int>& amount){
+        n = amount.size();
+        tree.resize(n);
         for(auto& e : edges){
-            graph[e[0]].push_back(e[1]);
-            graph[e[1]].push_back(e[0]);
+            tree[e[0]].push_back(e[1]);
+            tree[e[1]].push_back(e[0]);
         }
-        vector<int> cur = {bob};
-        vector<int> p = path(bob, -1, graph, cur);
-        map<int, int> bob_path;
-        for(int i = 0;i < p.size();i++){
-            int cur_v = p[i];
-            bob_path[cur_v] = i;
+        distance_from_bob.resize(n);
+        return findPaths(0, 0, 0, bob, amount);
+    }
+
+private:
+    vector<vector<int>> tree;
+    vector<int> distance_from_bob;
+    int n;
+    
+    int findPaths(int source, int parent, int time, int bob, vector<int>& amount){
+        int max_income = 0, max_child = INT_MIN;
+        if(source == bob){
+            distance_from_bob[source] = 0;
         }
-        queue<pair<int, int>> pending;
-        int time = 0, ans = INT_MIN;
-        vector<bool> vis(n, false);
-        pending.push({0, 0});
-        vis[0] = true;
-        while(!pending.empty()){
-            int sz = pending.size();
-            while(sz--){
-                int node = pending.front().first;
-                int income = pending.front().second;
-                pending.pop();
-                if(bob_path.find(node) != bob_path.end()){
-                    if(bob_path[node] == time){
-                        income += amount[node]/2;
-                    }
-                    else if(bob_path[node] > time){
-                        income += amount[node];
-                    }
-                }
-                else{
-                    income += amount[node];
-                }
-                for(auto ngbr: graph[node]){
-                    if(!vis[ngbr]){
-                        pending.push({ngbr, income});
-                        vis[ngbr] = true;
-                    }
-                }
-                if(graph[node].size() == 1 && node != 0){
-                    ans = max(ans, income);
-                }
+        else{
+            distance_from_bob[source] = n;
+        }
+        for(int adj : tree[source]){
+            if(adj != parent){
+                max_child = max(max_child, findPaths(adj, source, time + 1, bob, amount));
+                distance_from_bob[source] = min(distance_from_bob[source], distance_from_bob[adj] + 1);
             }
-            time++;
         }
-        return ans;
+        if(distance_from_bob[source] > time){
+            max_income += amount[source];
+        }
+        else if(distance_from_bob[source] == time){
+            max_income += amount[source] / 2;
+        }
+        if(max_child == INT_MIN){
+            return max_income;
+        }
+        else{
+            return max_income + max_child;
+        }
     }
 };
