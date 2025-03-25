249fc915f5018e0643722b53106302ef41186c26
Amrit Anand <78211070+AmritAnand-2003@users.noreply.github.com>
2025-03-25 18:37:56 +0530

    Time: 259 ms (47.48%), Space: 216.2 MB (64.77%) - LeetHub

--- 3657-check-if-grid-can-be-cut-into-sections/3657-check-if-grid-can-be-cut-into-sections.cpp
+++ 3657-check-if-grid-can-be-cut-into-sections/3657-check-if-grid-can-be-cut-into-sections.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     bool checkValidCuts(int n, vector<vector<int>>& rectangles) {
-        int m = rectangles.size();
-        vector<vector<int>> yaxis, xaxis;
         map<int, pair<int, int>> linex, liney;
-        for (auto r : rectangles) {
+        for (auto& r : rectangles) {
             linex[r[0]].first++;
             linex[r[2]].second++;
             liney[r[1]].first++;
             liney[r[3]].second++;
         }
         int active = 0, count = 0;
-        for(auto it : linex){
+        for(auto& it : linex){
             active -= it.second.second;
             if(it.second.second && active == 0){
                 count++;
@@ -22,7 +20,7 @@ public:
             }
         }
         active = 0, count = 0;
-        for (auto it : liney) {
+        for (auto& it : liney) {
             active -= it.second.second;
             if(it.second.second && active == 0){
                 count++;
