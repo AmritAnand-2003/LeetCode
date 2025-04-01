e2fb5d43574aa43ef41c70ddb411b57fed860c33
Amrit Anand <78211070+AmritAnand-2003@users.noreply.github.com>
2025-04-01 07:47:50 +0530

    Time: 22 ms (10.33%), Space: 211.6 MB (5%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     long long mostPoints(vector<vector<int>>& questions) {
-        int n = questions.size();
-        unordered_map<int, long long> ans;
-        for(int i = n - 1; i>= 0;i--){
-            ans[i] = questions[i][0] + ans[i + questions[i][1] + 1];
-            ans[i] = max(ans[i], ans[i + 1]);
+        vector<long long> ans(200001);
+        for(int i = questions.size() - 1; i>= 0;i--){
+            ans[i] = max(questions[i][0] + ans[i + questions[i][1] + 1], ans[i + 1]);
         }
         return ans[0];
     }
