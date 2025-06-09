1fcb18d45a99a3d48c4b08cd980893661918abf4
Amrit Anand <78211070+AmritAnand-2003@users.noreply.github.com>
2025-06-09 23:55:56 +0530

    Time: 0 ms (100%), Space: 7.8 MB (83.39%) - LeetHub

--- 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.cpp
+++ 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-    int nodeCount(long n, long n1, long n2){
+    int nodeCount(int& n, long n1, long n2){
         int cnt = 0;
         while(n1 <= n){
-            cnt += min(n + 1, n2) - n1;
+            cnt += min((long)n + 1, n2) - n1;
             n1 *= 10;
             n2 *= 10;
         }
@@ -14,7 +14,6 @@ public:
         k--;
         while(k > 0){
             int cnt = nodeCount(n, cur, cur + 1);
-            cout<<cnt<<" "<<k<<endl;
             if(k < cnt){
                 cur *= 10;
                 k--;
