6d285362b666d9fe00d3ad8221533f8e41bb1fff
Amrit Anand <78211070+AmritAnand-2003@users.noreply.github.com>
2025-08-13 00:47:53 +0530

    Time: 22 ms (83.23%), Space: 43.6 MB (69.34%) - LeetHub

---
+++ 1187-print-foobar-alternately/1187-print-foobar-alternately.java
@@ -0,0 +1,30 @@
+class FooBar {
+    private int n;
+    Semaphore runf, runb;
+
+    public FooBar(int n) {
+        this.n = n;
+        runf = new Semaphore(1);
+        runb = new Semaphore(0);
+    }
+
+    public void foo(Runnable printFoo) throws InterruptedException {
+        
+        for (int i = 0; i < n; i++) {
+            runf.acquire();
+        	// printFoo.run() outputs "foo". Do not change or remove this line.
+        	printFoo.run();
+            runb.release();
+        }
+    }
+
+    public void bar(Runnable printBar) throws InterruptedException {
+        
+        for (int i = 0; i < n; i++) {
+            runb.acquire();
+            // printBar.run() outputs "bar". Do not change or remove this line.
+        	printBar.run();
+            runf.release();
+        }
+    }
+}
